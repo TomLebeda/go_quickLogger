9175e91738b6ca1dbfe8160d040992e8ec5de48a
TomLebeda <tom.lebeda@gmail.com>
2022-08-12 17:53:07 +0200

    encapsulating all loggers into single struct

--- main.go
+++ main.go
@@ -7,15 +7,26 @@ import (
 	"time"
 )
 
+type LoggersStruct struct {
+	InfoLogger  *log.Logger
+	TraceLogger *log.Logger
+	WarnLogger  *log.Logger
+	ErrorLogger *log.Logger
+	DebugLogger *log.Logger
+	FatalLogger *log.Logger
+}
+
 var (
-	InfoLogger  = log.New(os.Stdout, fmt.Sprintf("%s %-8s", time.Now().Format("2006-01-02 15:04:05.000"), "INFO:"), 0)
-	TraceLogger = log.New(os.Stdout, fmt.Sprintf("%s %-8s", time.Now().Format("2006-01-02 15:04:05.000"), "TRACE:"), 0)
-	WarnLogger  = log.New(os.Stdout, fmt.Sprintf("%s %-8s", time.Now().Format("2006-01-02 15:04:05.000"), "WARN:"), 0)
-	ErrorLogger = log.New(os.Stdout, fmt.Sprintf("%s %-8s", time.Now().Format("2006-01-02 15:04:05.000"), "ERROR:"), 0)
-	DebugLogger = log.New(os.Stdout, fmt.Sprintf("%s %-8s", time.Now().Format("2006-01-02 15:04:05.000"), "DEBUG:"), 0)
-	FatalLogger = log.New(os.Stdout, fmt.Sprintf("%s %-8s", time.Now().Format("2006-01-02 15:04:05.000"), "FATAL:"), 0)
-	Tracing     = false
-	LogToFile   = false
+	Loggers = LoggersStruct{
+		InfoLogger:  log.New(os.Stdout, fmt.Sprintf("%s %-8s", time.Now().Format("2006-01-02 15:04:05.000"), "INFO:"), 0),
+		TraceLogger: log.New(os.Stdout, fmt.Sprintf("%s %-8s", time.Now().Format("2006-01-02 15:04:05.000"), "TRACE:"), 0),
+		WarnLogger:  log.New(os.Stdout, fmt.Sprintf("%s %-8s", time.Now().Format("2006-01-02 15:04:05.000"), "WARN:"), 0),
+		ErrorLogger: log.New(os.Stdout, fmt.Sprintf("%s %-8s", time.Now().Format("2006-01-02 15:04:05.000"), "ERROR:"), 0),
+		DebugLogger: log.New(os.Stdout, fmt.Sprintf("%s %-8s", time.Now().Format("2006-01-02 15:04:05.000"), "DEBUG:"), 0),
+		FatalLogger: log.New(os.Stdout, fmt.Sprintf("%s %-8s", time.Now().Format("2006-01-02 15:04:05.000"), "FATAL:"), 0),
+	}
+	Tracing   = false
+	LogToFile = false
 )
 
 func getLogFileName() string {
@@ -25,130 +36,130 @@ func getLogFileName() string {
 func Info(v ...interface{}) {
 	if LogToFile {
 		f, _ := os.OpenFile(getLogFileName(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		InfoLogger.SetOutput(f)
-		InfoLogger.Println(v...)
+		Loggers.InfoLogger.SetOutput(f)
+		Loggers.InfoLogger.Println(v...)
 		f.Close()
-		InfoLogger.SetOutput(os.Stdout)
+		Loggers.InfoLogger.SetOutput(os.Stdout)
 	}
-	InfoLogger.Println(v...)
+	Loggers.InfoLogger.Println(v...)
 }
 func Infof(format string, v ...interface{}) {
 	if LogToFile {
 		f, _ := os.OpenFile(getLogFileName(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		InfoLogger.SetOutput(f)
-		InfoLogger.Printf(format, v...)
+		Loggers.InfoLogger.SetOutput(f)
+		Loggers.InfoLogger.Printf(format, v...)
 		f.Close()
-		InfoLogger.SetOutput(os.Stdout)
+		Loggers.InfoLogger.SetOutput(os.Stdout)
 	}
-	InfoLogger.Printf(format, v...)
+	Loggers.InfoLogger.Printf(format, v...)
 }
 
 func Warn(v ...interface{}) {
 	if LogToFile {
 		f, _ := os.OpenFile(getLogFileName(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		WarnLogger.SetOutput(f)
-		WarnLogger.Println(v...)
+		Loggers.WarnLogger.SetOutput(f)
+		Loggers.WarnLogger.Println(v...)
 		f.Close()
-		WarnLogger.SetOutput(os.Stdout)
+		Loggers.WarnLogger.SetOutput(os.Stdout)
 	}
-	WarnLogger.Println(v...)
+	Loggers.WarnLogger.Println(v...)
 }
 func Warnf(format string, v ...interface{}) {
 	if LogToFile {
 		f, _ := os.OpenFile(getLogFileName(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		WarnLogger.SetOutput(f)
-		WarnLogger.Printf(format, v...)
+		Loggers.WarnLogger.SetOutput(f)
+		Loggers.WarnLogger.Printf(format, v...)
 		f.Close()
-		WarnLogger.SetOutput(os.Stdout)
+		Loggers.WarnLogger.SetOutput(os.Stdout)
 	}
-	WarnLogger.Printf(format, v...)
+	Loggers.WarnLogger.Printf(format, v...)
 }
 
 func Error(v ...interface{}) {
 	if LogToFile {
 		f, _ := os.OpenFile(getLogFileName(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		ErrorLogger.SetOutput(f)
-		ErrorLogger.Println(v...)
+		Loggers.ErrorLogger.SetOutput(f)
+		Loggers.ErrorLogger.Println(v...)
 		f.Close()
-		ErrorLogger.SetOutput(os.Stdout)
+		Loggers.ErrorLogger.SetOutput(os.Stdout)
 	}
-	ErrorLogger.Println(v...)
+	Loggers.ErrorLogger.Println(v...)
 }
 func Errorf(format string, v ...interface{}) {
 	if LogToFile {
 		f, _ := os.OpenFile(getLogFileName(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		ErrorLogger.SetOutput(f)
-		ErrorLogger.Printf(format, v...)
+		Loggers.ErrorLogger.SetOutput(f)
+		Loggers.ErrorLogger.Printf(format, v...)
 		f.Close()
-		ErrorLogger.SetOutput(os.Stdout)
+		Loggers.ErrorLogger.SetOutput(os.Stdout)
 	}
-	ErrorLogger.Printf(format, v...)
+	Loggers.ErrorLogger.Printf(format, v...)
 }
 
 func Fatal(v ...interface{}) {
 	if LogToFile {
 		f, _ := os.OpenFile(getLogFileName(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		FatalLogger.SetOutput(f)
-		FatalLogger.Println(v...)
+		Loggers.FatalLogger.SetOutput(f)
+		Loggers.FatalLogger.Println(v...)
 		f.Close()
-		FatalLogger.SetOutput(os.Stdout)
+		Loggers.FatalLogger.SetOutput(os.Stdout)
 	}
-	FatalLogger.Fatal(v...)
+	Loggers.FatalLogger.Fatal(v...)
 }
 
 func Fatalf(format string, v ...interface{}) {
 	if LogToFile {
 		f, _ := os.OpenFile(getLogFileName(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		FatalLogger.SetOutput(f)
-		FatalLogger.Printf(format, v...)
+		Loggers.FatalLogger.SetOutput(f)
+		Loggers.FatalLogger.Printf(format, v...)
 		f.Close()
-		FatalLogger.SetOutput(os.Stdout)
+		Loggers.FatalLogger.SetOutput(os.Stdout)
 	}
-	FatalLogger.Fatalf(format, v...)
+	Loggers.FatalLogger.Fatalf(format, v...)
 }
 func Trace(v ...interface{}) {
 	if Tracing {
 		if LogToFile {
 			f, _ := os.OpenFile(getLogFileName(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-			TraceLogger.SetOutput(f)
-			TraceLogger.Println(v...)
+			Loggers.TraceLogger.SetOutput(f)
+			Loggers.TraceLogger.Println(v...)
 			f.Close()
-			TraceLogger.SetOutput(os.Stdout)
+			Loggers.TraceLogger.SetOutput(os.Stdout)
 		}
-		TraceLogger.Println(v...)
+		Loggers.TraceLogger.Println(v...)
 	}
 }
 func Tracef(format string, v ...interface{}) {
 	if Tracing {
 		if LogToFile {
 			f, _ := os.OpenFile(getLogFileName(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-			TraceLogger.SetOutput(f)
-			TraceLogger.Printf(format, v...)
+			Loggers.TraceLogger.SetOutput(f)
+			Loggers.TraceLogger.Printf(format, v...)
 			f.Close()
-			TraceLogger.SetOutput(os.Stdout)
+			Loggers.TraceLogger.SetOutput(os.Stdout)
 		}
-		TraceLogger.Printf(format, v...)
+		Loggers.TraceLogger.Printf(format, v...)
 	}
 }
 
 func Debug(v ...interface{}) {
 	if LogToFile {
 		f, _ := os.OpenFile(getLogFileName(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		DebugLogger.SetOutput(f)
-		DebugLogger.Println(v...)
+		Loggers.DebugLogger.SetOutput(f)
+		Loggers.DebugLogger.Println(v...)
 		f.Close()
-		DebugLogger.SetOutput(os.Stdout)
+		Loggers.DebugLogger.SetOutput(os.Stdout)
 	}
-	DebugLogger.Println(v...)
+	Loggers.DebugLogger.Println(v...)
 }
 
 func Debugf(format string, v ...interface{}) {
 	if LogToFile {
 		f, _ := os.OpenFile(getLogFileName(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		DebugLogger.SetOutput(f)
-		DebugLogger.Printf(format, v...)
+		Loggers.DebugLogger.SetOutput(f)
+		Loggers.DebugLogger.Printf(format, v...)
 		f.Close()
-		DebugLogger.SetOutput(os.Stdout)
+		Loggers.DebugLogger.SetOutput(os.Stdout)
 	}
-	DebugLogger.Printf(format, v...)
+	Loggers.DebugLogger.Printf(format, v...)
 }
